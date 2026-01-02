8bf2cdd53f8e8943d0abae9a8fa93a3065660374
Oleh Kovalyshyn <kovalyshinO@gmail.com>
2026-01-02 18:48:45 +0200

    tobacco use analysis

--- workflow/tobacco.R
+++ workflow/tobacco.R
@@ -6,15 +6,14 @@ library(scales)
 source("utils/survey.R")
 
 data <- read.csv("data/ukr2019.csv")
+survey <- step2019(data)
 
 ##################################
 # SECTION 1. General tobacco use
 ##################################
 
 plot_general_tobacco_use <- function() {
-  data$smoker <- ifelse(data$t1 == 1, 1, 0)
-
-  survey <- step2019(data)
+  survey <- update(survey, smoker = ifelse(data$t1 == 1, 1, 0))
   print("Mean smoking prevalence:")
   svymean(~smoker, survey, na.rm = TRUE)
 
@@ -114,7 +113,19 @@ plot_general_vape_use <- function() {
 plot_urban_rural_vape_use <- function() {
   survey <- get_vape_and_thc_users(survey)
 
+  overall <- svymean(~vape_ths, survey, na.rm = TRUE)
+  overall <- data.frame(
+    ur = "Total",
+    vape_ths = coef(overall),
+    se = SE(overall)
+  )
+
   results <- svyby(~vape_ths, ~ur, survey, svymean, na.rm = TRUE)
+  results <- bind_rows(results, overall)
+  results$ur <- factor(
+    results$ur,
+    levels = c("Rural", "Urban", "Total")
+  )
 
   ggplot(results, aes(x = ur, y = vape_ths)) +
     geom_col(position = position_dodge(width = 0.8)) +
@@ -145,3 +156,88 @@ plot_urban_rural_vape_use <- function() {
 
 plot_general_vape_use()
 plot_urban_rural_vape_use()
+
+##################################
+# SECTION 3. Age differences
+##################################
+
+get_tobacco_preferences_by_age <- function(age_value = "") {
+  subset <- update(
+    survey,
+    agerange = factor(
+      agerange,
+      levels = c("18-29", "30-44", "45-59", "60-69")
+    )
+  )
+
+  if (age_value != "") {
+    subset <- subset(subset, agerange == age_value)
+  }
+
+  subset <- update(
+    subset,
+    cigs = ifelse(!is.na(t1a) & t1a == 1, 1, 0),
+    ths = ifelse(!is.na(t1b) & t1b == 1, 1, 0),
+    vape = ifelse(!is.na(t1c) & t1c == 1, 1, 0),
+    hookah = ifelse(!is.na(t1d) & t1d == 1, 1, 0),
+    snuff = ifelse(!is.na(t1e) & t1e == 1, 1, 0)
+  )
+
+  results <- svymean(~cigs + ths + vape + hookah + snuff, subset, na.rm = TRUE)
+  return(results)
+}
+
+plot_tobacco_by_age <- function() {
+    age_groups <- c("18-29", "30-44", "45-59", "60-69", "")
+    age_labels <- c("18-29", "30-44", "45-59", "60-69", "18-69")
+
+    all_results <- data.frame()
+
+    for (i in seq_along(age_groups)) {
+        agerange <- age_groups[i]
+        label <- age_labels[i]
+
+        results <- get_tobacco_preferences_by_age(agerange)
+        results_df <- data.frame(
+          product = names(results),
+          prevalence = coef(results),
+          se = SE(results),
+          agerange = label
+        )
+
+        all_results <- bind_rows(all_results, results_df)
+    }
+
+    all_results$product <- factor(
+        all_results$product,
+        levels = c("cigs", "ths", "vape", "hookah", "snuff"),
+        labels = c("Cigarettes", "THS", "Vape", "Hookah", "Snuff")
+    )
+
+    all_results$agerange <- factor(
+      all_results$agerange,
+      levels = c("18-29", "30-44", "45-59", "60-69", "18-69")
+    )
+
+    ggplot(all_results, aes(x = agerange, y = prevalence, fill = product)) +
+        geom_col(position = position_dodge(width = 0.8)) +
+        geom_text(
+          aes(label = scales::percent(prevalence, accuracy = 0.01)),
+          position = position_dodge(width = 0.8),
+          vjust = -0.3,
+          size = 3.5
+        ) +
+        scale_y_continuous(
+          labels = percent_format(accuracy = 1),
+          limits = c(0, 0.5)
+        ) +
+        labs(
+          x = "Tobacco product",
+          y = "Prevalence",
+          fill = "Age group",
+          title = "Tobacco product preferences by age group",
+          subtitle = "Survey-weighted estimates with 95% confidence intervals"
+        )
+}
+
+plot_tobacco_by_age()
