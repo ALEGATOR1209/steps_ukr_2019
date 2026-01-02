d3f901242dd9739d2e1967589db4e694929cf56b
Oleh Kovalyshyn <kovalyshinO@gmail.com>
2026-01-02 13:07:34 +0200

    add correction by age+sex

--- utils/survey.R
+++ utils/survey.R
@@ -1,7 +1,10 @@
-derzhstat_age_brackets <- function () {
-  derzhstat_demographics <- read.csv("data/000_0204.csv", sep = "\t")
-  derzhstat_demographics <- derzhstat_demographics[, -c(1, 2)]
+library(dplyr)
+library(survey)
+
+load_derzhstat_file <- function(path) {
+  derzhstat_demographics <- read.csv(path, sep = "\t")
   names(derzhstat_demographics) <- c("age", "population")
+
   derzhstat_demographics <- derzhstat_demographics %>% mutate(
     age = as.integer(as.character(age)),
     population = as.integer(as.character(population))
@@ -13,10 +16,22 @@ derzhstat_age_brackets <- function () {
                           right = FALSE,
                           labels = c("18-29", "30-44", "45-59", "60-69"))) %>%
     group_by(agerange) %>%
-    summarise(total_pop = sum(population, na.rm = TRUE)) %>%
-    mutate(prop = total_pop / sum(total_pop))
+    summarise(total_pop = sum(population, na.rm = TRUE))
 
   return(derzhstat_demographics)
+}
+
+derzhstat_age_brackets <- function () {
+  male <- load_derzhstat_file("data/derzhstat_2019_male.csv")
+  female <- load_derzhstat_file("data/derzhstat_2019_female.csv")
+
+  male$sex <- "Men"
+  female$sex <- "Women"
+  result <- bind_rows(male, female)
+  result$bracket <- paste(result$sex, result$agerange, sep = "_")
+  result <- result %>% mutate(prop = total_pop / sum(total_pop))
+
+  return(result)
 }
 
 step2019 <- function (data) {
@@ -29,11 +44,18 @@ step2019 <- function (data) {
   )
 
   demographics <- derzhstat_age_brackets()
+  pop <- demographics$prop
+  names(pop) <- demographics$bracket
+
+  survey <- update(
+    survey,
+    bracket = paste(sex, agerange, sep = "_")
+  )
 
   survey <- svystandardize(
     design = survey,
-    by = ~agerange,
+    by = ~bracket,
     over = ~1,
-    population = demographics$prop
+    population = pop
   )
 }
--- workflow/tobacco.R
+++ workflow/tobacco.R
@@ -1,10 +1,54 @@
-library(dplyr)
 library(survey)
+library(ggplot2)
+library(forcats)
+library(scales)
+
 source("utils/survey.R")
 
 data <- read.csv("data/ukr2019.csv")
 data$smoker <- ifelse(data$t1 == 1, 1, 0)
 
 survey <- step2019(data)
-results <- svyby(~smoker, ~agerange, survey, svymean, na.rm = TRUE)
+print("Mean smoking prevalence:")
+svymean(~smoker, survey, na.rm = TRUE)
+
+print("Smoking prevalence over sex and age:")
+results <- bind_rows(
+  svyby(~smoker, ~sex + agerange, survey, svymean, na.rm = TRUE),
+  svyby(~smoker, ~sex, survey, svymean, na.rm = TRUE) %>% mutate(agerange = "18-69")
+)
+
+results$agerange <- factor(
+  results$agerange,
+  levels = c("18-29", "30-44", "45-59", "60-69", "18-69")
+)
+
 print(results)
+
+ggplot(results, aes(x = agerange, y = smoker, fill = sex)) +
+  geom_col(position = position_dodge(width = 0.8)) +
+  geom_errorbar(
+    aes(
+      ymin = smoker - 1.96 * se,
+      ymax = smoker + 1.96 * se
+    ),
+    position = position_dodge(width = 0.8),
+    width = 0.2
+  ) +
+  geom_text(
+    aes(label = scales::percent(smoker, accuracy = 0.01)),
+    position = position_dodge(width = 1.2),
+    vjust = -0.3,
+    size = 3.5
+  ) +
+  scale_y_continuous(
+    labels = percent_format(accuracy = 1),
+    limits = c(0, 1)
+  ) +
+  labs(
+    x = "Age group",
+    y = "Smoking prevalence",
+    fill = "Sex",
+    title = "Smoking prevalence by sex and age",
+    subtitle = "Survey-weighted estimates with 95% confidence intervals"
+  )
